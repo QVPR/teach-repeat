384b82b72a8ce284cebe853eae1759c968c058b7
Dominic D <dominic.dallosto@qut.edu.au>
2020-03-10 12:28:56 +1000

    Basic ball detection and turning to face

--- catkin_ws/src/miro_demos/src/ball_detection.py
+++ catkin_ws/src/miro_demos/src/ball_detection.py
@@ -1,65 +1,104 @@
 #!/usr/bin/python
 
 import rospy
-from sensor_msgs.msg import CompressedImage, Image
+from sensor_msgs.msg import CompressedImage, Image, JointState
+from geometry_msgs.msg import TwistStamped
 
 import cv2
 import cv_bridge
 import numpy as np
+import math
+
+import miro2 as miro
 
 class miro_ball_detection:
 
-    def __init__(self):
-        self.bridge = cv_bridge.CvBridge()
-        # subscribe to the touch sensor message
-        self.sub_image = rospy.Subscriber("/miro/sensors/caml/compressed", CompressedImage, self.got_image_data, queue_size=1)
-        # publish motor commands
-        self.pub_image = rospy.Publisher("/miro/filtered_image", Image, queue_size=0)
+	def __init__(self):
+		self.bridge = cv_bridge.CvBridge()
+		# subscribe to the image from the camera
+		
+		self.sub_image = rospy.Subscriber("/miro/sensors/caml/compressed", CompressedImage, self.got_image_data, queue_size=1)
+		# publish the processed image
+		self.pub_image = rospy.Publisher("/miro/filtered_image", Image, queue_size=0)
 
-    def got_image_data(self, msg):
-        try:
-            image = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
-        except cv_bridge.CvBridgeError as e:
-            print(e)
-            return
+		# publish the desired joint states of Miro (so it looks downwards to view the ball)
+		self.pub_joints = rospy.Publisher("/miro/control/kinematic_joints", JointState, queue_size=0)
+		self.joint_states = JointState()
+		self.joint_states.name = ['tilt','lift','yaw','pitch']
+		self.joint_states.position = [0.0, miro.constants.LIFT_RAD_MAX, 0.0, miro.constants.PITCH_RAD_MAX]
 
-        image = self.filter_image(image)
-        
-        try:
-            self.pub_image.publish(self.bridge.cv2_to_imgmsg(image, "mono8"))
-        except cv_bridge.CvBridgeError as e:
-            print(e)
+		# publish motor commands
+		self.pub_cmd_vel = rospy.Publisher("/miro/control/cmd_vel", TwistStamped, queue_size=0)
+		
 
-    def filter_image(self, image):
-        image = np.float32(image)
+	def got_image_data(self, msg):
+		try:
+			image = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
+		except cv_bridge.CvBridgeError as e:
+			print(e)
+			return
 
-        # gamma correction
-        image = image * 1/255
-        cv2.pow(image, 2.2, image)
+		image = self.filter_image(image)
+		
+		try:
+			self.pub_image.publish(self.bridge.cv2_to_imgmsg(image, "bgr8"))
+		except cv_bridge.CvBridgeError as e:
+			print(e)
 
-        blue, green, red = cv2.split(image)
-        combined = cv2.add(blue, cv2.add(green, red))
+	def filter_image(self, image):
+		image = np.float32(image)
 
-        # chromacity
-        red = cv2.divide(red, combined)
-        green = cv2.divide(green, combined)
-        blue = cv2.divide(blue, combined)
+		# gamma correction
+		image = image * 1/255
+		cv2.pow(image, 2.2, image)
 
-        yellow = cv2.add(green, red)
+		blue, green, red = cv2.split(image)
+		combined = blue + green + red
 
-        _, yellowT = cv2.threshold(yellow, 0.9, 1.0, cv2.THRESH_BINARY)
-        yellowT = cv2.morphologyEx(yellowT, cv2.MORPH_CLOSE,np.ones((3,3),np.float))
-        yellowT = cv2.morphologyEx(yellowT, cv2.MORPH_OPEN,np.ones((5,5),np.float))
+		# chromacity
+		red = cv2.divide(red, combined)
+		green = cv2.divide(green, combined)
+		blue = cv2.divide(blue, combined)
 
-        return np.uint8(yellowT * 255)
+		_, redT = cv2.threshold(red, 180.0/255.0, 1.0, cv2.THRESH_BINARY)
+		_, blueT = cv2.threshold(blue, 30.0/255.0, 1.0, cv2.THRESH_BINARY)
+		mask = redT * blueT
+
+		mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE,np.ones((5,5),np.float))
+		mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN,np.ones((3,3),np.float))
+
+		chroma = cv2.merge([blue*mask, green*mask, red*mask])
+
+		points = np.argwhere(mask == 1.0)[:,::-1] # need to reverse x and y
+		medXY = np.uint32(np.median(points,0))
+
+		cv2.line(image, (medXY[0]-5,medXY[1]), (medXY[0]+5,medXY[1]), (1,0,0))
+		cv2.line(image, (medXY[0],medXY[1]-5), (medXY[0],medXY[1]+5), (1,0,0))
+
+		turn_signal = TwistStamped()
+		turn_signal.header.stamp = rospy.Time.now()
+		if medXY[0] < (0.7 * image.shape[1]):
+			turn_signal.twist.angular.z = 1.0
+		elif medXY[0] > (0.9 * image.shape[1]):
+			turn_signal.twist.angular.z = -1.0
+		self.pub_cmd_vel.publish(turn_signal)
+
+		return np.uint8(image * 255)
+
+	def publish_joint_state(self):
+		self.joint_states.header.stamp = rospy.Time.now()
+		self.pub_joints.publish(self.joint_states)
 
 
 
 
 if __name__ == "__main__":
-    # intialise this program as a ROS node
-    rospy.init_node("miro_ball_detection")
-    # start the demo program
-    demo = miro_ball_detection()
-    # spin makes the program run while ROS is running
-    rospy.spin()
+	# intialise this program as a ROS node
+	rospy.init_node("miro_ball_detection")
+	# start the demo program
+	demo = miro_ball_detection()
+	# publish the desired joint states at 50 Hz
+	rate = rospy.Rate(50)
+	while not rospy.is_shutdown():
+		demo.publish_joint_state()
+		rate.sleep()
--- catkin_ws/src/miro_demos/src/touch_control.py
+++ catkin_ws/src/miro_demos/src/touch_control.py
@@ -7,57 +7,57 @@ from std_msgs.msg import UInt16
 from geometry_msgs.msg import TwistStamped
 
 class BodyTouchSensor(Enum):
-    mid_front = 0
-    mid_front_mid = 1
-    mid_back_mid = 2
-    mid_back = 3
-    right_front = 4
-    right_front_mid = 5
-    right_mid = 6
-    right_back_mid = 7
-    right_back = 8
-    left_front = 9
-    left_front_mid = 10
-    left_mid = 11
-    left_back_mid = 12
-    left_back = 13
+	mid_front = 0
+	mid_front_mid = 1
+	mid_back_mid = 2
+	mid_back = 3
+	right_front = 4
+	right_front_mid = 5
+	right_mid = 6
+	right_back_mid = 7
+	right_back = 8
+	left_front = 9
+	left_front_mid = 10
+	left_mid = 11
+	left_back_mid = 12
+	left_back = 13
 
 class miro_touch_control:
 
-    def __init__(self):
-        # subscribe to the touch sensor message
-        self.sub_touch_body = rospy.Subscriber("/miro/sensors/touch_body", UInt16, self.got_touch_sensor_data, queue_size=1)
-        # publish motor commands
-        self.pub_cmd_vel = rospy.Publisher("/miro/control/cmd_vel", TwistStamped, queue_size=0)
+	def __init__(self):
+		# subscribe to the touch sensor message
+		self.sub_touch_body = rospy.Subscriber("/miro/sensors/touch_body", UInt16, self.got_touch_sensor_data, queue_size=1)
+		# publish motor commands
+		self.pub_cmd_vel = rospy.Publisher("/miro/control/cmd_vel", TwistStamped, queue_size=0)
 
-    def got_touch_sensor_data(self, msg):
-        # partly documented here: http://labs.consequentialrobotics.com/miro-e/docs/index.php?page=Technical_Interfaces_ROS
-        # and from experimentation with the GUI - data is a 16 bit int where the lower 14 bits give the state of each body sensor
-        # put the touch sensor values in a nice format so we can query them:
-        touch_sensor = {}
-        for element in BodyTouchSensor:
-            touch_sensor[element.name] = bool(msg.data & (1 << element.value))
+	def got_touch_sensor_data(self, msg):
+		# partly documented here: http://labs.consequentialrobotics.com/miro-e/docs/index.php?page=Technical_Interfaces_ROS
+		# and from experimentation with the GUI - data is a 16 bit int where the lower 14 bits give the state of each body sensor
+		# put the touch sensor values in a nice format so we can query them:
+		touch_sensor = {}
+		for element in BodyTouchSensor:
+			touch_sensor[element.name] = bool(msg.data & (1 << element.value))
 
-        # depending on the touch sensor readings, set the velocity command message for Miro
-        velocity_message = TwistStamped()
-        velocity_message.header.stamp = rospy.Time.now()
+		# depending on the touch sensor readings, set the velocity command message for Miro
+		velocity_message = TwistStamped()
+		velocity_message.header.stamp = rospy.Time.now()
 
-        if touch_sensor["mid_front"] or touch_sensor["mid_front_mid"]:
-            velocity_message.twist.linear.x = 0.2
-        elif touch_sensor["mid_back"] or touch_sensor["mid_back_mid"]:
-            velocity_message.twist.linear.x = -0.2
+		if touch_sensor["mid_front"] or touch_sensor["mid_front_mid"]:
+			velocity_message.twist.linear.x = 0.2
+		elif touch_sensor["mid_back"] or touch_sensor["mid_back_mid"]:
+			velocity_message.twist.linear.x = -0.2
 
-        if touch_sensor["left_mid"] or touch_sensor["left_front_mid"] or touch_sensor["left_back_mid"]:
-            velocity_message.twist.angular.z = 2
-        elif touch_sensor["right_mid"] or touch_sensor["right_front_mid"] or touch_sensor["right_back_mid"]:
-            velocity_message.twist.angular.z = -2
+		if touch_sensor["left_mid"] or touch_sensor["left_front_mid"] or touch_sensor["left_back_mid"]:
+			velocity_message.twist.angular.z = 2
+		elif touch_sensor["right_mid"] or touch_sensor["right_front_mid"] or touch_sensor["right_back_mid"]:
+			velocity_message.twist.angular.z = -2
 
-        self.pub_cmd_vel.publish(velocity_message)
+		self.pub_cmd_vel.publish(velocity_message)
 
 if __name__ == "__main__":
-    # intialise this program as a ROS node
-    rospy.init_node("miro_touch_control")
-    # start the demo program
-    demo = miro_touch_control()
-    # spin makes the program run while ROS is running
-    rospy.spin()
+	# intialise this program as a ROS node
+	rospy.init_node("miro_touch_control")
+	# start the demo program
+	demo = miro_touch_control()
+	# spin makes the program run while ROS is running
+	rospy.spin()
