7060623d5a27f9e9fef92f8b16aeb80298cf750a
Dominic D <dominic.dallosto@qut.edu.au>
2020-03-18 10:27:23 +1000

    Simplified odometry integration code using PyKDL libs

--- catkin_ws/src/miro_teach_repeat/src/odom_integrator.py
+++ catkin_ws/src/miro_teach_repeat/src/odom_integrator.py
@@ -5,11 +5,6 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Pose
 import tf_conversions
 
-import cv2
-import cv_bridge
-import numpy as np
-import math
-
 class miro_integrate_odom:
 
 	def __init__(self):		
@@ -21,40 +16,34 @@ class miro_integrate_odom:
 
 		self.last_odom_time = None
 		self.odom_pose = Pose()
+		self.odom_pose.orientation.w = 1
 
 	def process_odom_data(self, msg):
 		if self.last_odom_time is None:
 			self.last_odom_time = msg.header.stamp
 			return
+
 		delta_time = (msg.header.stamp - self.last_odom_time).to_sec()
 		self.last_odom_time = msg.header.stamp
 
-		# current yaw angle
-		theta = tf_conversions.transformations.euler_from_quaternion(self.quaternion_to_numpy(self.odom_pose.orientation))[2]
+		# convert current pose and odometry to PyKDL format for calculations
+		pose_frame = tf_conversions.fromMsg(self.odom_pose)
+		velocity = tf_conversions.Twist(
+			tf_conversions.Vector(msg.twist.twist.linear.x ,msg.twist.twist.linear.y, msg.twist.twist.linear.z),
+			tf_conversions.Vector(msg.twist.twist.angular.x ,msg.twist.twist.angular.y, msg.twist.twist.angular.z)
+		)
 
-		# calculate velocity
-		delta_x = msg.twist.twist.linear.x * delta_time * math.cos(theta)
-		delta_y = msg.twist.twist.linear.x * delta_time * math.sin(theta)
-		delta_theta = msg.twist.twist.angular.z * delta_time
+		pose_frame.Integrate(velocity, 1/delta_time)
 
-		# update Pose
-		self.odom_pose.position.x += delta_x
-		self.odom_pose.position.y += delta_y
-		theta += delta_theta
-		self.odom_pose.orientation = tf_conversions.Quaternion(*tf_conversions.transformations.quaternion_from_euler(0, 0, theta + delta_theta))
+		self.odom_pose = tf_conversions.toMsg(pose_frame)
 
-		# publish integrated odometry messages
 		odom_to_publish = msg
 		odom_to_publish.pose.pose = self.odom_pose
 		self.pub_odom.publish(odom_to_publish)
 
-	def quaternion_to_numpy(self, quaternion):
-		return np.array([quaternion.x, quaternion.y, quaternion.z, quaternion.w])
-
 
 
 if __name__ == "__main__":
-
 	rospy.init_node("miro_integrate_odom")
 	integrator = miro_integrate_odom()
 	rospy.spin()
