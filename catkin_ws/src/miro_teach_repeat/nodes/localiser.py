438466eeb02e7c5d768ae859e52d2f3dd202ecad
Dominic D <dominic.dallosto@qut.edu.au>
2020-03-24 15:41:30 +1000

    Fixed bugs with camera setup and controlling the head position

--- catkin_ws/src/miro_teach_repeat/nodes/localiser.py
+++ catkin_ws/src/miro_teach_repeat/nodes/localiser.py
@@ -6,8 +6,9 @@ import cv2
 import os
 import pickle
 import datetime
+import time
 from rospy_message_converter import message_converter
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import CompressedImage, JointState
 from std_msgs.msg import String
 
 import math
@@ -17,7 +18,7 @@ import image_processing
 from miro_teach_repeat.srv import ImageMatch
 
 
-DEFAULT_CAMERA_SETTINGS = "frame=180w@25"
+DEFAULT_CAMERA_SETTINGS = "frame=180w@15"
 
 class miro_localiser:
 
@@ -34,17 +35,36 @@ class miro_localiser:
 
 		# publish camera settings
 		self.pub_camera_settings = rospy.Publisher("/miro/control/command", String, queue_size=0)
-		self.camera_settings = rospy.get_param('~camera_setup_command', DEFAULT_CAMERA_SETTINGS)
-		self.pub_camera_settings.publish(String(data=self.camera_settings))
+		self.camera_settings = String(data=rospy.get_param('~camera_setup_command', DEFAULT_CAMERA_SETTINGS))
+
+		# publish the desired joint states of Miro (so it looks downwards to view the ball)
+		self.pub_joints = rospy.Publisher("/miro/control/kinematic_joints", JointState, queue_size=0)
+		self.joint_states = JointState()
+		self.joint_states.name = ['tilt','lift','yaw','pitch']
+		self.joint_states.position = [0.0, math.radians(45), 0.0, math.radians(8)]
 		
 	def process_image(self, msg):
 		normalised_image = image_processing.patch_normalise_msg(msg, (9,9), compressed=True, resize=self.resize)
 		delta_pose = self.match_image(image_processing.image_to_msg(normalised_image))
 		# TODO - steer based on this pose difference
 
+	def publish_joint_state(self):
+		self.joint_states.header.stamp = rospy.Time.now()
+		self.pub_joints.publish(self.joint_states)
+
+	def publish_camera_command(self):
+		self.pub_camera_settings.publish(self.camera_settings)
+
 
 if __name__ == "__main__":
 
 	rospy.init_node("miro_localiser")
-	integrator = miro_localiser()
-	rospy.spin()
+	localiser = miro_localiser()
+	# hacky but seems we need to sleep for a bit before sending this command
+	time.sleep(1)
+	localiser.publish_camera_command()
+	rate = rospy.Rate(50)
+	while not rospy.is_shutdown():
+		localiser.publish_joint_state()
+		rate.sleep()
+
