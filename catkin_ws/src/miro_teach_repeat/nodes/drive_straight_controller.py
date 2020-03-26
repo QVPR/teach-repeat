bc066ae311288bce46edbaf1c411080b8dea547b
Dominic D <dominic.dallosto@qut.edu.au>
2020-03-26 10:40:43 +1000

    Made controller to drive Miro straight

---
+++ catkin_ws/src/miro_teach_repeat/nodes/drive_straight_controller.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+from enum import Enum
+
+import rospy
+from geometry_msgs.msg import TwistStamped
+from nav_msgs.msg import Odometry
+import tf_conversions
+
+class miro_drive_straight_controller:
+
+	def __init__(self):
+		self.setup_parameters()
+		self.setup_publishers()
+		self.setup_subscribers()
+
+	def setup_parameters(self):
+		self.gain_distance = rospy.get_param('~gain_distance', 5.0)
+		self.gain_turn = rospy.get_param('~gain_turn', 2.0)
+
+	def setup_publishers(self):
+		self.pub_cmd_vel = rospy.Publisher("/miro/control/cmd_vel", TwistStamped, queue_size=0)
+
+	def setup_subscribers(self):
+		self.sub_odom = rospy.Subscriber("/miro/odom_integrated", Odometry, self.process_odom_data, queue_size=1)
+
+	def process_odom_data(self, msg):
+		current_frame = tf_conversions.fromMsg(msg.pose.pose)
+		d = current_frame.p.y()
+		theta = current_frame.M.GetRPY()[2]
+
+		turn_command = -self.gain_distance * d - self.gain_turn * theta
+
+		motor_command = TwistStamped()
+		motor_command.header.stamp = rospy.Time.now()
+		motor_command.twist.linear.x = 0.1
+		motor_command.twist.angular.z = turn_command
+
+		self.pub_cmd_vel.publish(motor_command)
+
+if __name__ == "__main__":
+	rospy.init_node("miro_drive_straight_controller")
+	controller = miro_drive_straight_controller()
+	rospy.spin()
