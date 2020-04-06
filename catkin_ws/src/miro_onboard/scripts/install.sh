300d2d6df5a01ae9a76df44f6e2e065d6f245882
Dominic D <dominic.dallosto@qut.edu.au>
2020-04-06 12:14:10 +1000

    Created installer for onboard code

---
+++ catkin_ws/src/miro_onboard/scripts/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Miro IP: " ip
+echo "Copying files..."
+scp -r "$(readlink -f $(dirname "$0")/..)" "miro@$ip:/home/miro/mdk/catkin_ws/src"
+echo "Building files..."
+ssh -t "miro@$ip" "cd ~/mdk/catkin_ws && source devel/setup.bash && catkin_make"
+echo "Configuring startup behaviour..."
+ssh -t "miro@$ip" "cd ~/.miro2/config && echo 'roslaunch miro_onboard onboard.launch &' > on_system_ready.sh && chmod +x on_system_ready.sh"
