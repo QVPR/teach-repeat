80aa533a766a08068d6bccd67be572b6fd8d634c
Dominic D <dominic.dallosto@qut.edu.au>
2020-04-20 15:46:52 +1000

    Fixed bug in miro_onboard install script to allow clean install.

--- catkin_ws/src/miro_onboard/scripts/install.sh
+++ catkin_ws/src/miro_onboard/scripts/install.sh
@@ -4,6 +4,6 @@ read -p "Miro IP: " ip
 echo "Copying files..."
 scp -r "$(readlink -f $(dirname "$0")/..)" "miro@$ip:/home/miro/mdk/catkin_ws/src"
 echo "Building files..."
-ssh -t "miro@$ip" "cd ~/mdk/catkin_ws && source devel/setup.bash && catkin_make"
+ssh -t "miro@$ip" "cd ~/mdk/catkin_ws && source /opt/ros/kinetic/setup.bash && catkin_make"
 echo "Configuring startup behaviour..."
 ssh -t "miro@$ip" "cd ~/.miro2/config && echo 'source ~/mdk/catkin_ws/devel/setup.bash && roslaunch --wait miro_onboard onboard.launch &' > on_system_ready.sh && chmod +x on_system_ready.sh"
