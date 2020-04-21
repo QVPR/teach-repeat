95aebf43cceea4d31d859e460f35ea019db9ee01
Dominic D <dominic.dallosto@qut.edu.au>
2020-04-22 09:48:14 +1000

    Fixed bug with image message synchronisation

--- catkin_ws/src/miro_teach_repeat/nodes/data_collect.py
+++ catkin_ws/src/miro_teach_repeat/nodes/data_collect.py
@@ -40,9 +40,9 @@ class miro_data_collect:
 		self.sub_odom = rospy.Subscriber("/miro/sensors/odom/integrated", Odometry, self.process_odom_data, queue_size=1)
 
 		# subscribe to the images from both cameras
-		self.sub_image_left = message_filters.Subscriber("/miro/sensors/cam/left/compressed", CompressedImage, queue_size=1)
-		self.sub_image_right = message_filters.Subscriber("/miro/sensors/cam/right/compressed", CompressedImage, queue_size=1)
-		self.sub_images = message_filters.ApproximateTimeSynchronizer((self.sub_image_left, self.sub_image_right), 5, 1.0/30.0)
+		self.sub_image_left = message_filters.Subscriber("/miro/sensors/cam/left/compressed", CompressedImage, queue_size=10)
+		self.sub_image_right = message_filters.Subscriber("/miro/sensors/cam/right/compressed", CompressedImage, queue_size=10)
+		self.sub_images = message_filters.ApproximateTimeSynchronizer((self.sub_image_left, self.sub_image_right), 100, 1.0/30.0)
 		self.sub_images.registerCallback(self.process_image_data)
 		
 	def process_odom_data(self, msg):
