b130c0c0a2d8d8baf4a9890e79fbd19cad1d5f98
Dominic D <dominic.dallosto@qut.edu.au>
2020-04-22 17:13:08 +1000

    Did more testing to find the root of the image syncing issue

--- scripts/stamp_test.py
+++ scripts/stamp_test.py
@@ -4,6 +4,7 @@ import rospy
 from sensor_msgs.msg import CompressedImage
 import tf_conversions
 import message_filters
+from std_msgs.msg import Float64
 
 import math
 import time
@@ -21,47 +22,41 @@ class miro_stamp_test:
 		self.N_sync = 0
 		self.N_L = 0
 		self.N_R = 0
+		self.time_offset = 0
 
 	def setup_publishers(self):	
-		pass
+		self.pub_delta_t_real = rospy.Publisher('/miro/delta_t_real', Float64, queue_size=1)
 
 	def setup_subscribers(self):
-		self.sub_left = rospy.Subscriber("/miro/sensors/cam/left/compressed", CompressedImage, self.process_left_image_data)
-		self.sub_left = rospy.Subscriber("/miro/sensors/cam/right/compressed", CompressedImage, self.process_right_image_data)
-
 		# subscribe to the images from both cameras
-		self.sub_image_left = message_filters.Subscriber("/miro/sensors/cam/left/compressed", CompressedImage, queue_size=10)
-		self.sub_image_right = message_filters.Subscriber("/miro/sensors/cam/right/compressed", CompressedImage, queue_size=10)
-		self.sub_images = message_filters.ApproximateTimeSynchronizer((self.sub_image_left, self.sub_image_right), 100, 1.0/30.0)
+		self.sub_image_left = message_filters.Subscriber("/miro/sensors/cam/left/compressed", CompressedImage, queue_size=1, buffer_size=2**24)
+		self.sub_image_right = message_filters.Subscriber("/miro/sensors/cam/right/compressed", CompressedImage, queue_size=1, buffer_size=2**24)
+		self.sub_images = message_filters.ApproximateTimeSynchronizer((self.sub_image_left, self.sub_image_right), 10, 1.0/30.0)
 		self.sub_images.registerCallback(self.process_image_data)
+		self.sub_image_left.registerCallback(self.process_left_image_data)
+		self.sub_image_right.registerCallback(self.process_right_image_data)
 		
 	def process_left_image_data(self, msg):
 		self.N_L += 1
-		stamp = msg.header.stamp
-		if self.last_right_stamp is not None:
-			print('[%d] L - %f [diff = %f s]' % (self.N_L, stamp.to_sec(), (stamp - self.last_right_stamp).to_sec()))
-		else:
-			print('[%d] L - %f' % (self.N_L, msg.header.stamp.to_sec()))
-		self.last_left_stamp = stamp
+		if self.N_L == 1:
+			self.time_offset = rospy.Time.now().to_sec() - msg.header.stamp.to_sec()
 
 	def process_right_image_data(self, msg):
 		self.N_R += 1
-		stamp = msg.header.stamp
-		if self.last_left_stamp is not None:
-			print('[%d] R - %f [diff = %f s]' % (self.N_R, stamp.to_sec(), (stamp - self.last_left_stamp).to_sec()))
-		else:
-			print('[%d] R - %f' % (self.N_R, msg.header.stamp.to_sec()))
-		self.last_right_stamp = stamp
 
 	def process_image_data(self, msg_left, msg_right):
 		self.N_sync += 1
-		print('%d: diff = %f. Q = [%d,%d]' % (self.N_sync, abs(msg_left.header.stamp - msg_right.header.stamp).to_sec(),len(self.sub_images.queues[0]),len(self.sub_images.queues[1])))
+		Qstr = '[' + ','.join([str(len(q)) for q in self.sub_images.queues]) + ']'
+		print('%d: Q=%s N=[%d,%d]' % (self.N_sync,Qstr,self.N_L,self.N_R))
+		delta_t_real = rospy.Time.now().to_sec() - msg_left.header.stamp.to_sec() - self.time_offset
+		print('delta t real = %f s' % delta_t_real)
+		self.pub_delta_t_real.publish(Float64(data=delta_t_real))
 
 	def stop(self):
 		print('left [%d], right [%d], synched [%d]' % (self.N_L, self.N_R, self.N_sync))
 
 if __name__ == "__main__":
-	rospy.init_node("img_stamp_diff")
-	collector = miro_stamp_test()
-	rospy.on_shutdown(collector.stop)
+	rospy.init_node("img_stamp_tester")
+	stamp_tester = miro_stamp_test()
+	rospy.on_shutdown(stamp_tester.stop)
 	rospy.spin()
