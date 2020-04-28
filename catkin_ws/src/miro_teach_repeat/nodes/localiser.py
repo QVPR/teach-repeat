24c3d8d5c6b7c2026e9a98932beb2a0c7792ba5a
Dominic D <dominic.dallosto@qut.edu.au>
2020-04-28 16:36:40 +1000

    Removed odom_follower (not part of localiser)

--- catkin_ws/src/miro_teach_repeat/nodes/localiser.py
+++ catkin_ws/src/miro_teach_repeat/nodes/localiser.py
@@ -4,28 +4,46 @@ import rospy
 import numpy as np
 import cv2
 import os
-import pickle
-import datetime
-import time
 import math
 import json
 from rospy_message_converter import message_converter
-from sensor_msgs.msg import CompressedImage, JointState
-from std_msgs.msg import Float64
-import message_filters
+from sensor_msgs.msg import CompressedImage
+from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Odometry
+import tf_conversions
 
 import image_processing
 from miro_onboard.msg import CompressedImageSynchronised
 from miro_teach_repeat.srv import ImageMatch
-from miro_teach_repeat.srv import PoseOffset
-class miro_localiser:
 
+def read_file(filename):
+	with open(filename, 'r') as f:
+		data = f.read()
+		return data
+
+def load_poses(pose_files):
+	return [message_converter.convert_dictionary_to_ros_message('geometry_msgs/Pose',json.loads(read_file(f))) for f in pose_files]
+
+class miro_localiser:
 	def __init__(self):
 		self.setup_parameters()
 		self.setup_publishers()
 		self.setup_subscribers()
 
-	def setup_parameters(self):	
+	def setup_parameters(self):
+		# Odom
+		self.load_dir = os.path.expanduser(rospy.get_param('miro_data_load_dir', '~/miro/data'))
+		if self.load_dir[-1] != '/':
+			self.load_dir += '/'
+
+		pose_files = [self.load_dir+f for f in os.listdir(self.load_dir) if f[-9:] == '_pose.txt']
+		pose_files.sort()
+
+		self.poses = load_poses(pose_files)
+		self.goal_index = 0
+		self.goal = self.poses[self.goal_index]
+
+		# Image
 		self.resize = image_processing.make_size(height=rospy.get_param('/image_resize_height', None), width=rospy.get_param('/image_resize_width', None))
 		if self.resize[0] is None and self.resize[1] is None:
 			self.resize = None
@@ -44,33 +62,63 @@ class miro_localiser:
 			os.makedirs(self.save_dir+'full/')
 		if not os.path.isdir(self.save_dir+'norm/'):
 			os.makedirs(self.save_dir+'norm/')
-
-		self.Nleft = 0
-		self.Nright = 0
-		self.last_stamp = None
-		self.time_offset = 0
 		
 	def setup_publishers(self):	
+		self.goal_pub = rospy.Publisher('/miro/control/goal', PoseStamped, queue_size=1)
+
 		rospy.wait_for_service('/miro/match_image')
 		self.match_image = rospy.ServiceProxy('/miro/match_image', ImageMatch, persistent=True)
-		self.pub_delta_t_real = rospy.Publisher('/miro/delta_t_real', Float64, queue_size=1)
 
 	def setup_subscribers(self):
-		# subscribe to the images from both cameras
+		self.sub_odom = rospy.Subscriber("/miro/sensors/odom/integrated", Odometry, self.process_odom_data, queue_size=1)
 		self.sub_images = rospy.Subscriber('/miro/sensors/cam/both/compressed', CompressedImageSynchronised, self.process_image_data, queue_size=1, buff_size=2**22)
-		self.service = rospy.Service('/miro/get_image_pose_offset', PoseOffset, self.calculate_image_pose_offset)
+
+	def process_odom_data(self, msg):
+		current_frame_odom = tf_conversions.fromMsg(msg.pose.pose)
+		current_goal_frame_odom = tf_conversions.fromMsg(self.goal)
+		old_goal_frame_world = tf_conversions.fromMsg(self.poses[self.goal_index])
+
+		delta_frame = current_frame_odom.Inverse() * current_goal_frame_odom
+
+		if delta_frame.p.Norm() < 0.10:
+			old_goal_index = self.goal_index
+			self.goal_index += 1
+			if self.goal_index == len(self.poses):
+				self.goal_index = len(self.poses)-1 # stop at the end
+				# self.goal_index = 0 # repeat the path (loop)
+
+				image_theta_offset = self.calculate_image_pose_offset(old_goal_index)
+				new_goal_frame_world = tf_conversions.fromMsg(self.poses[self.goal_index])
+
+				# old target -> new target in frame of old target (x = forwards)
+				goal_offset = old_goal_frame_world.Inverse() * new_goal_frame_world
+				# rotate to odom frame (x = positive odom) [same frame as current_goal]
+				goal_offset.p = tf_conversions.Rotation.RotZ(current_goal_frame_odom.M.GetRPY()[2]) * goal_offset.p
+				# rotate the goal offset by the rotational correction
+				goal_offset_corrected = tf_conversions.Frame(tf_conversions.Rotation.RotZ(image_theta_offset)) * goal_offset
+				# add the corrected offset to the current goal
+				new_goal_odom = tf_conversions.Frame(goal_offset_corrected.M * current_goal_frame_odom.M, goal_offset_corrected.p + current_goal_frame_odom.p)
+
+				self.goal = tf_conversions.toMsg(new_goal_odom)
+				self.publish_goal(self.goal, 0.1)
+			else:
+				self.goal = self.poses[self.goal_index]
+				self.publish_goal(self.goal, 0.1)
+
+	def publish_goal(self, pose, lookahead_distance=0.0):
+		goal = PoseStamped()
+		goal.header.stamp = rospy.Time.now()
+		goal.header.frame_id = "odom"
+		theta = tf_conversions.fromMsg(pose).M.GetRPY()[2]
+		goal.pose.position.x = pose.position.x + lookahead_distance * math.cos(theta)
+		goal.pose.position.y = pose.position.y + lookahead_distance * math.sin(theta)
+		goal.pose.orientation = pose.orientation
+		self.goal_pub.publish(goal)
 
 	def process_image_data(self, msg):
 		n = msg.left.header.seq
 		if self.last_image is None:
 			self.first_img_seq = n
-		# if self.last_stamp is not None:
-			# print('delta t = %f s' % (msg.left.header.stamp - self.last_stamp).to_sec())
-			# delta_t_real = rospy.Time.now().to_sec() - msg.left.header.stamp.to_sec() - self.time_offset
-			# print('delta t real = %f s' % delta_t_real)
-			# self.pub_delta_t_real.publish(Float64(data=delta_t_real))
-		# else: 
-			# self.time_offset = (rospy.Time.now() - msg.left.header.stamp).to_sec()
 		n -= self.first_img_seq
 
 		full_image = image_processing.stitch_stereo_image_message(msg.left, msg.right, compressed=True)
@@ -80,22 +128,21 @@ class miro_localiser:
 		cv2.imwrite(self.save_dir+('norm/%06d.png' % n), np.uint8(255.0 * (1 + normalised_image) / 2.0))
 
 		self.last_image = normalised_image
-		# self.last_stamp = msg.left.header.stamp
 
-	def calculate_image_pose_offset(self, srv):
+	def calculate_image_pose_offset(self, image_to_search_index):
 		if self.last_image is not None:
-			image_offset = self.match_image(image_processing.image_to_msg(self.last_image), srv.goalIndex).pixelOffset.data
+			image_offset = self.match_image(image_processing.image_to_msg(self.last_image), image_to_search_index).pixelOffset.data
 
 			# positive image offset: query image is shifted left from reference image
 			# this means we have done a right (negative turn) which we should correct with a positive turn
 			# positive offset -> positive turn, gain = positive
 			# (normalise the pixel offset by the width of the image)
-			return Float64(data=self.image_offset_gain * float(image_offset) / self.last_image.shape[1])
+			return self.image_offset_gain * float(image_offset) / self.last_image.shape[1]
 		else:
 			raise RuntimeError('Localiser: tried to localise before image data is received!')
-
 
 if __name__ == "__main__":
 	rospy.init_node("miro_localiser")
 	localiser = miro_localiser()
+	localiser.publish_goal(localiser.poses[0])
 	rospy.spin()
