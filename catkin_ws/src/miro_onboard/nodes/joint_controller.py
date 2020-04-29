40262da24b8a29bcce7a6ffaf21bf3c4b2c3913d
Dominic D <dominic.dallosto@qut.edu.au>
2020-04-29 17:11:04 +1000

    Fixed control of Miro's kinematic joints Incorporates P controller so shouldn't get stuck at the wrong value

--- catkin_ws/src/miro_onboard/nodes/joint_controller.py
+++ catkin_ws/src/miro_onboard/nodes/joint_controller.py
@@ -18,17 +18,20 @@ class miro_joint_controller:
 	def setup_parameters(self):
 		self.joint_states = None
 		self.enabled = False
+		self.K_P = 0.2
 
 	def setup_publishers(self):
-		self.pub_joints = rospy.Publisher("/miro/control/kinematic_joints", JointState, queue_size=0)
+		self.pub_joints = rospy.Publisher("/miro/control/kinematic_joints", JointState, queue_size=1)
 
 	def setup_subscribers(self):
+		self.sub_joints = rospy.Subscriber("/miro/sensors/kinematic_joints", JointState, self.process_joint_data, queue_size=1)
 		self.srv_set_joint_states = rospy.Service('miro/control/kinematic_joints/set_fixed_state', SetJointState, self.set_joint_state)
 		self.srv_enable_set_joint_states = rospy.Service('miro/control/kinematic_joints/fixed/enable', Trigger, self.enable_set_joint_state)
 		self.srv_disable_set_joint_states = rospy.Service('miro/control/kinematic_joints/fixed/disable', Trigger, self.disable_set_joint_state)
 
 	def set_joint_state(self, srv):
 		self.joint_states = srv.jointStates
+		print(self.joint_states.position)
 		return (True, "")
 	
 	def enable_set_joint_state(self, srv):
@@ -43,22 +46,21 @@ class miro_joint_controller:
 		self.enabled = False
 		return (True, "")
 
-	def publish_joint_state(self):
+	def process_joint_data(self, msg):
 		if self.enabled:
+			errors = [x-y for x, y in zip(self.joint_states.position, msg.position)]
+			errors = [error if abs(error) > 0.1 else 0.0 for error in errors]
+			set_joint_positions = [joint_position + self.K_P*error + random.random()*0.01 for error, joint_position in zip(errors, self.joint_states.position)]
+
 			joint_state_command = JointState()
 			joint_state_command.header.stamp = rospy.Time.now()
 			joint_state_command.name = self.joint_states.name
-			joint_state_command.position = [joint_pos + 0.001*random.random() for joint_pos in self.joint_states.position]
+			joint_state_command.position = set_joint_positions
 			self.pub_joints.publish(joint_state_command)
 
 
 if __name__ == "__main__":
-
 	rospy.init_node("miro_joint_controller")
 	controller = miro_joint_controller()
-	
-	rate = rospy.Rate(50)
-	while not rospy.is_shutdown():
-		controller.publish_joint_state()
-		rate.sleep()
+	rospy.spin()
 
