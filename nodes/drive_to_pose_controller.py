1f6af135cc4f5f7e8b6a4e6239eaaf5abd4bb0eb
Dominic D <dominic.dallosto@qut.edu.au>
2020-04-30 09:53:21 +1000

    Made nodes wait for setup to finish before starting

--- nodes/drive_to_pose_controller.py
+++ nodes/drive_to_pose_controller.py
@@ -3,6 +3,7 @@
 import rospy
 from geometry_msgs.msg import TwistStamped
 from nav_msgs.msg import Odometry
+from std_msgs.msg import Bool
 import tf_conversions
 
 import math
@@ -23,6 +24,7 @@ class miro_drive_to_pose_controller:
 		self.setup_subscribers()
 
 	def setup_parameters(self):
+		self.ready = not rospy.get_param('/wait_for_ready', False)
 		self.gain_rho = rospy.get_param('~gain_distance', 0.5)
 		self.gain_alpha = rospy.get_param('~gain_turn_to_point', 5.0)
 		self.gain_beta = rospy.get_param('~gain_turn_to_heading', -3.0)
@@ -34,42 +36,49 @@ class miro_drive_to_pose_controller:
 		self.pub_cmd_vel = rospy.Publisher("/miro/control/cmd_vel", TwistStamped, queue_size=1)
 
 	def setup_subscribers(self):
+		if not self.ready:
+			self.sub_ready = rospy.Subscriber("/miro/ready", Bool, self.on_ready, queue_size=1)
 		self.sub_odom = rospy.Subscriber("/miro/sensors/odom/integrated", Odometry, self.process_odom_data, queue_size=1)
 		self.sub_goal = rospy.Subscriber("/miro/control/goal", Goal, self.set_goal, queue_size=1)
 
+	def on_ready(self, msg):
+		if msg.data:
+			self.ready = True
+
 	def process_odom_data(self, msg):
-		if self.goal_pos is None:
-			return
+		if self.ready:
+			if self.goal_pos is None:
+				return
 
-		current_frame = tf_conversions.fromMsg(msg.pose.pose)
-		theta = current_frame.M.GetRPY()[2]
+			current_frame = tf_conversions.fromMsg(msg.pose.pose)
+			theta = current_frame.M.GetRPY()[2]
 
-		d_pos = tf_conversions.Vector(*self.goal_pos) - current_frame.p
-		rho = d_pos.Norm()
-		alpha = wrapToPi(math.atan2(d_pos.y(), d_pos.x()) - theta)
-		beta = wrapToPi(-theta - alpha + self.goal_theta)
+			d_pos = tf_conversions.Vector(*self.goal_pos) - current_frame.p
+			rho = d_pos.Norm()
+			alpha = wrapToPi(math.atan2(d_pos.y(), d_pos.x()) - theta)
+			beta = wrapToPi(-theta - alpha + self.goal_theta)
 
-		v = self.gain_rho * rho
-		omega = self.gain_alpha * alpha + self.gain_beta * beta
+			v = self.gain_rho * rho
+			omega = self.gain_alpha * alpha + self.gain_beta * beta
 
-		# Scale to preserve rate of turn
-		if not self.stop_at_goal or abs(v) > MAX_V or abs(omega) > MAX_OMEGA:
-			turn_rate = abs(omega / v)
-			turn_rate_at_max = MAX_OMEGA / MAX_V
+			# Scale to preserve rate of turn
+			if not self.stop_at_goal or abs(v) > MAX_V or abs(omega) > MAX_OMEGA:
+				turn_rate = abs(omega / v)
+				turn_rate_at_max = MAX_OMEGA / MAX_V
 
-			if turn_rate > turn_rate_at_max:
-				omega = MAX_OMEGA * omega / abs(omega)
-				v = MAX_OMEGA / turn_rate * v / abs(v)
-			else:
-				omega = MAX_V * turn_rate * omega / abs(omega)
-				v = MAX_V * v / abs(v)
+				if turn_rate > turn_rate_at_max:
+					omega = MAX_OMEGA * omega / abs(omega)
+					v = MAX_OMEGA / turn_rate * v / abs(v)
+				else:
+					omega = MAX_V * turn_rate * omega / abs(omega)
+					v = MAX_V * v / abs(v)
 
-		motor_command = TwistStamped()
-		motor_command.header.stamp = rospy.Time.now()
-		motor_command.twist.linear.x = v
-		motor_command.twist.angular.z = omega
+			motor_command = TwistStamped()
+			motor_command.header.stamp = rospy.Time.now()
+			motor_command.twist.linear.x = v
+			motor_command.twist.angular.z = omega
 
-		self.pub_cmd_vel.publish(motor_command)
+			self.pub_cmd_vel.publish(motor_command)
 
 	def set_goal(self, msg):
 		self.goal_pos = [msg.pose.pose.position.x, msg.pose.pose.position.y, 0]
--- nodes/teleop_joy.py
+++ nodes/teleop_joy.py
@@ -5,6 +5,7 @@ from enum import Enum
 import rospy
 from geometry_msgs.msg import TwistStamped
 from sensor_msgs.msg import Joy
+from std_msgs.msg import Bool
 
 class miro_teleop_joy:
 
@@ -14,24 +15,32 @@ class miro_teleop_joy:
 		self.setup_subscribers()
 
 	def setup_parameters(self):
+		self.ready = not rospy.get_param('/wait_for_ready', False)
 		self.axis_linear = rospy.get_param('~axis_linear', 1)
 		self.axis_angular = rospy.get_param('~axis_angular', 2)
 		self.scale_linear = rospy.get_param('~scale_linear', 0.2)
 		self.scale_angular = rospy.get_param('~scale_angular', 1.0)
 
 	def setup_publishers(self):
-		self.pub_cmd_vel = rospy.Publisher("/miro/control/cmd_vel", TwistStamped, queue_size=0)
+		self.pub_cmd_vel = rospy.Publisher("/miro/control/cmd_vel", TwistStamped, queue_size=1)
 
 	def setup_subscribers(self):
+		if not self.ready:
+			self.sub_ready = rospy.Subscriber("/miro/ready", Bool, self.on_ready, queue_size=1)
 		self.sub_odom = rospy.Subscriber("/joy", Joy, self.process_joy_data, queue_size=1)
 
+	def on_ready(self, msg):
+		if msg.data:
+			self.ready = True
+	
 	def process_joy_data(self, msg):
-		motor_command = TwistStamped()
-		motor_command.header.stamp = rospy.Time.now()
-		motor_command.twist.linear.x = msg.axes[self.axis_linear] * self.scale_linear
-		motor_command.twist.angular.z = msg.axes[self.axis_angular] * self.scale_angular
+		if self.ready:
+			motor_command = TwistStamped()
+			motor_command.header.stamp = rospy.Time.now()
+			motor_command.twist.linear.x = msg.axes[self.axis_linear] * self.scale_linear
+			motor_command.twist.angular.z = msg.axes[self.axis_angular] * self.scale_angular
 
-		self.pub_cmd_vel.publish(motor_command)
+			self.pub_cmd_vel.publish(motor_command)
 
 if __name__ == "__main__":
 	rospy.init_node("miro_teleop_joy")
