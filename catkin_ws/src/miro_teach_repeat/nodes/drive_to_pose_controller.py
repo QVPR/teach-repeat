0540b3273bf1e90a223ffdcb5ef0aa4df413d040
Dominic D <dominic.dallosto@qut.edu.au>
2020-08-12 12:23:28 +1000

    Merged across changes from Miro updates

--- catkin_ws/src/miro_teach_repeat/nodes/drive_to_pose_controller.py
+++ catkin_ws/src/miro_teach_repeat/nodes/drive_to_pose_controller.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import rospy
-from geometry_msgs.msg import TwistStamped, Twist
+from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Bool
 import tf_conversions
@@ -20,6 +20,14 @@ def rho_alpha_beta(dx, dy, theta, goal_theta):
 	beta = wrapToPi(-theta - alpha + goal_theta)
 	return rho, alpha, beta
 
+def check_control_parameters(gain_rho, gain_alpha, gain_beta):
+	if gain_rho <= 0:
+		print('Drive to pose controller: Control unstable! gain_rho must be > 0')
+	if gain_beta >= 0:
+		print('Drive to pose controller: Control unstable! gain_beta must be < 0')
+	if gain_alpha - gain_rho <= 0:
+		print('Drive to pose controller: Control unstable! gain_alpha must be > gain_rho')
+
 
 class drive_to_pose_controller:
 
@@ -34,7 +42,7 @@ class drive_to_pose_controller:
 		self.gain_alpha = rospy.get_param('~gain_turn_to_point', 5.0)
 		self.gain_beta = rospy.get_param('~gain_turn_to_heading', -3.0)
 		self.gain_theta = rospy.get_param('~gain_turn_on_spot', 7.0)
-		self.zero_odom_offset = None
+		check_control_parameters(self.gain_rho, self.gain_alpha, self.gain_beta)
 		self.max_v = rospy.get_param('~max_v', 0.2)
 		self.min_omega = rospy.get_param('~min_omega', 0.15)
 		self.max_omega = rospy.get_param('~max_omega', 0.93)
@@ -44,7 +52,6 @@ class drive_to_pose_controller:
 		self.turning_goal_distance = rospy.get_param('/goal_pose_seperation', 0.2) * rospy.get_param('/turning_target_range_distance_ratio', 0.5)
 
 	def setup_publishers(self):
-		# self.pub_cmd_vel = rospy.Publisher("cmd_vel", TwistStamped, queue_size=1)
 		self.pub_cmd_vel = rospy.Publisher("cmd_vel", Twist, queue_size=1)
 
 	def setup_subscribers(self):
@@ -80,20 +87,10 @@ class drive_to_pose_controller:
 		if msg.data:
 			self.ready = True
 
-	def subtract_odom(self, odom, odom_frame_to_subtract):
-		odom_frame = tf_conversions.fromMsg(odom.pose.pose) 
-		subtracted_odom = odom_frame_to_subtract.Inverse() * odom_frame
-		odom.pose.pose = tf_conversions.toMsg(subtracted_odom)
-		return odom
-
 	def process_odom_data(self, msg):
 		if self.ready:
 			if self.goal_pos is None:
 				return
-
-			if self.zero_odom_offset is None:
-				self.zero_odom_offset = tf_conversions.fromMsg(msg.pose.pose)
-			msg = self.subtract_odom(msg, self.zero_odom_offset)
 
 			current_frame = tf_conversions.fromMsg(msg.pose.pose)
 			theta = current_frame.M.GetRPY()[2]
@@ -101,22 +98,19 @@ class drive_to_pose_controller:
 			d_pos = tf_conversions.Vector(*self.goal_pos) - current_frame.p
 			rho, alpha, beta = rho_alpha_beta(d_pos.x(), d_pos.y(), theta, self.goal_theta)
 
-			v = self.gain_rho * rho
-			omega = self.gain_alpha * alpha + self.gain_beta * beta
-
-			if rho < self.turning_goal_distance:
+			if rho < self.turning_goal_distance or alpha > math.pi/2 and alpha < -math.pi/2:
+				# we're at a turning goal, so we should just turn
+				# or we're not in the stable region of the controller - rotate to face the goal
+				# 	better would be to reverse the direction of motion, but this would break along-path localisation
 				v = 0
 				omega = self.gain_theta * wrapToPi(self.goal_theta-theta)
+			else:
+				# we're in the stable region of the controller
+				v = self.gain_rho * rho
+				omega = self.gain_alpha * alpha + self.gain_beta * beta
 
 			v, omega = self.scale_velocities(v, omega, self.stop_at_goal)
 
-			# if rho < 0.1:
-			# 	print('only turning - omega = %f' % omega)
-
-			# motor_command = TwistStamped()
-			# motor_command.header.stamp = rospy.Time.now()
-			# motor_command.twist.linear.x = v
-			# motor_command.twist.angular.z = omega
 			motor_command = Twist()
 			motor_command.linear.x = v
 			motor_command.angular.z = omega
--- catkin_ws/src/miro_teach_repeat/scripts/odom_compare.py
+++ catkin_ws/src/miro_teach_repeat/scripts/odom_compare.py
@@ -10,6 +10,7 @@ import math
 import sys
 import matplotlib.pyplot as plt
 from rospy_message_converter import message_converter
+from geometry_msgs.msg import Pose
 import tf_conversions
 
 def read_file(filename):
@@ -18,31 +19,44 @@ def read_file(filename):
 	return data
 
 def get_pose_files(dir):
-	pose_files = [dir+f for f in os.listdir(dir) if f[-9:] == '_pose.txt']
+	pose_files = [dir+f for f in os.listdir(dir) if f.endswith('_pose.txt')]
+	pose_files.sort()
+	return pose_files
+
+def get_ground_truth_files(dir):
+	pose_files = [dir+f for f in os.listdir(dir) if f.endswith('_map_to_base_link.txt')]
 	pose_files.sort()
 	return pose_files
 
 def read_pose_files(pose_files):
 	return [tf_conversions.fromMsg(message_converter.convert_dictionary_to_ros_message('geometry_msgs/Pose',json.loads(read_file(p)))) for p in pose_files]
 
+def read_transform_stamped_files(pose_files):
+	transforms = [message_converter.convert_dictionary_to_ros_message('geometry_msgs/TransformStamped',json.loads(read_file(p))).transform for p in pose_files]
+	return [tf_conversions.fromMsg(Pose(tf.translation,tf.rotation)).Inverse() for tf in transforms]
+
+def set_first_pose_to_zero(poses):
+	first_pose_inverse = poses[0].Inverse()
+	return [first_pose_inverse * pose for pose in poses]
+
 def get_pose_x_y_theta(poses):
-	
 	x = np.array([pose.p.x() for pose in poses])
 	y = np.array([pose.p.y() for pose in poses])
 	theta = np.array([pose.M.GetRPY()[2] for pose in poses])
 	return x, y, theta
 
 #### #### ####
-base_dir = os.path.expanduser('~/miro/data/')
-dir_name = 'office5/'
-odom_dirs = [base_dir + dir_name]
+odom_dirs = ['/home/dominic/Desktop/2020-08-05_11:14:24/']
+odom_dirs = [os.path.expanduser(dir) for dir in odom_dirs]
 
 pose_files = [get_pose_files(dir) for dir in odom_dirs]
-poses = [read_pose_files(p) for p in pose_files]
+ground_truth_files = [get_ground_truth_files(dir) for dir in odom_dirs]
+poses = [set_first_pose_to_zero(read_pose_files(p)) for p in pose_files]
+ground_truths = [set_first_pose_to_zero(read_transform_stamped_files(p)) for p in ground_truth_files]
 
-pose_data = [get_pose_x_y_theta(pose) for pose in poses]
+pose_data = [get_pose_x_y_theta(pose) for pose in poses] + [get_pose_x_y_theta(pose) for pose in ground_truths]
 
-colours = ['#00ff00', '#0000ff', '#ff0000']
+colours = ['#44dd44', '#4444dd', '#dd4444']
 
 for pose_data_list,colour in zip(pose_data,colours):
 	plt.quiver(pose_data_list[0],pose_data_list[1],np.cos(pose_data_list[2]),np.sin(pose_data_list[2]), scale=50, color=colour)
@@ -51,6 +65,8 @@ for pose_data_list,colour in zip(pose_data,colours):
 # plt.scatter([3.5,3.5], [0,-2], s=100, c='#000000', marker='x')
 # plt.scatter([7.2,7.2,4.8,1.2], [0,-1.8,-1.8,-1.8], s=100, c='#000000', marker='x')
 # plt.scatter([3.6], [0], s=100, c='#000000', marker='x')
-plt.title('Outdoors')
+# plt.title('Outdoors')
+plt.legend(['odom','slam'])
 plt.axis('equal')
+plt.title('Jackal teach run: odometry vs SLAM trajectory')
 plt.show()
