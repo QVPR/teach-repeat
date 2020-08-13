32244937b291c74dc2e9f52044ed54621f4bfe83
Dominic D <dominic.dallosto@qut.edu.au>
2020-08-13 16:52:48 +1000

    Fixed issue if wait_for_ready or global_localisation_init were false

--- catkin_ws/src/miro_teach_repeat/nodes/data_save.py
+++ catkin_ws/src/miro_teach_repeat/nodes/data_save.py
@@ -24,6 +24,8 @@ class data_save:
 		self.ready = not rospy.get_param('/wait_for_ready', False)
 		self.save_id = 0
 		self.save_dir = os.path.expanduser(rospy.get_param('~save_dir', '~/miro/data'))
+		self.save_full_res_images = rospy.get_param('/save_full_res_images', True)
+		self.save_gt_data = rospy.get_param('/save_gt_data', True)
 		self.timestamp_dir = rospy.get_param('~timestamp_folder', False)
 		if self.save_dir[-1] != '/':
 			self.save_dir += '/'
@@ -31,8 +33,9 @@ class data_save:
 			self.save_dir += datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S/')
 		if not os.path.isdir(self.save_dir):
 			os.makedirs(self.save_dir)
-		if not os.path.isdir(self.save_dir+'full/'):
-			os.makedirs(self.save_dir+'full/')
+		if self.save_full_res_images:
+			if not os.path.isdir(self.save_dir+'full/'):
+				os.makedirs(self.save_dir+'full/')
 		if not os.path.isdir(self.save_dir+'norm/'):
 			os.makedirs(self.save_dir+'norm/')
 
@@ -74,16 +77,18 @@ class data_save:
 			normalised_image = image_processing.patch_normalise_image(image, self.patch_size, resize=self.resize)
 			message_as_text = json.dumps(message_converter.convert_ros_message_to_dictionary(pose))
 
-			try:
-				trans = self.tfBuffer.lookup_transform('/base_link', '/map', rospy.Time())
-				trans_as_text = json.dumps(message_converter.convert_ros_message_to_dictionary(trans))
-				with open(self.save_dir + id + '_map_to_base_link.txt', 'w') as pose_file:
-					pose_file.write(trans_as_text)
-			except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-				print('Could not lookup transform from /map to /base_link')
-				pass
-
-			cv2.imwrite(self.save_dir+'full/'+id+'.png', image)
+			if self.save_gt_data:
+				try:
+					trans = self.tfBuffer.lookup_transform('map', 'base_link', rospy.Time())
+					trans_as_text = json.dumps(message_converter.convert_ros_message_to_dictionary(trans))
+					with open(self.save_dir + id + '_map_to_base_link.txt', 'w') as pose_file:
+						pose_file.write(trans_as_text)
+				except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+					print('Could not lookup transform from /map to /base_link')
+					pass
+			
+			if self.save_full_res_images:
+				cv2.imwrite(self.save_dir+'full/'+id+'.png', image)
 			cv2.imwrite(self.save_dir+'norm/'+id+'.png', np.uint8(255.0 * (1 + normalised_image) / 2.0))
 			with open(self.save_dir+id+'_pose.txt', 'w') as pose_file:
 				pose_file.write(message_as_text)
