ab04d861662b8914449bcf5cc38d4e8423f07636
Tobias Fischer <tobias.fischer@qut.edu.au>
2020-10-05 09:34:32 +1000

    Update save_ground_truth.py

--- nodes/save_ground_truth.py
+++ nodes/save_ground_truth.py
@@ -6,6 +6,7 @@ import json
 from rospy_message_converter import message_converter
 import tf
 import tf2_ros
+from miro_teach_repeat.srv import SetGoal, SetGoalResponse
 
 
 class teach_repeat_ground_truth_save:
@@ -23,7 +24,6 @@ class teach_repeat_ground_truth_save:
 			os.makedirs(self.save_dir)
 		if not os.path.isdir(self.save_dir+'pose/'):
 			os.makedirs(self.save_dir+'pose/')
-		self.goal_number = 0
 		
 	def setup_publishers(self):	
 		pass
@@ -31,16 +31,19 @@ class teach_repeat_ground_truth_save:
 	def setup_subscribers(self):
 		self.tfBuffer = tf2_ros.Buffer()
 		self.tfListener = tf2_ros.TransformListener(self.tfBuffer)
+		rospy.sleep(0.2)
+		self.service = rospy.Service('set_goal', SetGoal, self.save_tf_data)
 
-	def save_tf_data(self, pose, goal_odom, goal_world, theta_offset, path_offset):
+	def save_tf_data(self, req):
 		try:
 			trans = self.tfBuffer.lookup_transform('map', 'base_link', rospy.Time())
 			trans_as_text = json.dumps(message_converter.convert_ros_message_to_dictionary(trans))
-			with open(self.save_dir + ('pose/%06d_map_to_base_link.txt' % self.goal_number), 'w') as pose_file:
+			with open(self.save_dir + ('pose/%06d_map_to_base_link.txt' % req.goalnum), 'w') as pose_file:
 				pose_file.write(trans_as_text)
+			return SetGoalResponse(success=True)
 		except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
 			print('Could not lookup transform from /map to /base_link')
-			pass
+			return SetGoalResponse(success=False)
 
 
 if __name__ == "__main__":
